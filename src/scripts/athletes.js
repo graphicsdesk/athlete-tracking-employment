89ac6907dbbf76a5c9096125d5a5abd6aed66180
Charlotte Li <li.zihao@columbia.edu>
2020-11-13 20:48:26 -0500

    changed vocab in data

--- src/scripts/athletes.js
+++ src/scripts/athletes.js
@@ -4,7 +4,7 @@ import graph from '../../data/athletes.json';
 const counts = `industry,BASE,MBB,MXC/TF,MFENC,FB,MOLF,LROW,HROW,MOC,MSQU,MSWIM,MENNIS,WREST,ARCH,WBB,WFENC,FH,WOLF,LAX,WROW,WOCCER,SOFTBALL,WSQUA,WSWIM,WENNIS,WTF/XC,VB
 Finance,14,4,11,2,27,3,14,11,10,9,9,9,11,4,5,3,6,1,9,5,4,3,3,8,4,6,5
 Graduate School,2,0,4,4,6,0,3,3,1,1,1,1,2,2,3,1,3,0,4,5,3,2,0,4,0,7,0
-Tech,0,1,2,0,4,1,0,3,2,1,6,0,2,0,0,0,0,1,0,0,3,0,0,0,1,3,3
+Technology,0,1,2,0,4,1,0,3,2,1,6,0,2,0,0,0,0,1,0,0,3,0,0,0,1,3,3
 Athletics,2,4,1,1,5,1,2,0,4,1,0,0,3,1,1,2,1,1,0,1,2,0,2,0,0,0,0
 Building,1,0,1,0,7,0,0,2,1,0,0,0,6,0,0,0,0,0,0,0,1,0,0,0,0,0,0
 Research,0,0,2,2,0,1,0,1,1,0,0,0,0,0,0,3,3,0,1,0,1,0,1,3,0,2,0
